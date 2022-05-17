ee472cc6b4713636f9590e4b60b5a01ff02c77e7
RiverRay <laolei@forkway.cn>
2022-05-17 22:56:59 +0800

    feat(exercise): 4,7,11,13,14

--- questions/00014-easy-first/template.ts
+++ questions/00014-easy-first/template.ts
@@ -1,1 +1,3 @@
-type First<T extends any[]> = any
+// type First<T extends any[]> = T extends [infer F, ... infer _R ] ? F : never
+// type First<T extends any[]> = T extends[] ? never: T[0]
+type First<T extends any[]> = T extends { 0: infer F } ? F : never
